42c6bfd6909ee47b87ea6ee6c9ce46364bb0164f
David De La Vega <ddelavega2015@gmail.com>
2022-06-13 00:10:05 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//1. Http is Hypertext Transfer Protocal and it is how the browser and server communicate with each other
+//2. URL is an address to an online source
+//3. DNS is Domain Name System which turns URLs into IP addresses
+//4. A query string allows arguments to be passed to the URL
+//5. Two HTTP Verbs are GET and POST. GET gets data from the server and POST sends data to the server
+//6. A HTTP request is when the browser requests something from the server
+//7. A HTTP response is when the server responds back to the browser request
+//8. Headers are more information on the request/response. Some examples are Cookies, Date, Last time modified
+//9. The processes that happen are the URL gets converted into an IP address, the browser sends a request to the server, the server responds to the request, and then the browser creates a DOM.  
